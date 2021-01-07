3a51e7718762b7ef7bd8eb721f7e9f2b232f66cf
zManuu <60520810+zManuu@users.noreply.github.com>
2021-01-07 12:26:38 +0100

    pc compatibility was added

--- Calculator/script.js
+++ Calculator/script.js
@@ -15,21 +15,45 @@ function replace(s, c, w) {
 function startup() {
   if (window.screen.width > 500) {
     print("PC detected!");
-    try { document.getElementById("inputField").size = 80; } catch (i) {}
+
+    // Set size of 'inputField'
+    try {
+      document.getElementById("inputField").size = 60;
+      document.getElementById("outputField").size = 20;
+    } catch (i) {}
+
+    // Linebreaks
+    for (var i = 0; i < document.getElementsByTagName("br").length; i++) {
+      var m = document.getElementsByTagName("br")[i];
+      print(i + ": " + m.className);
+      if (m.className != "brPC") {
+        m.hidden = true;
+      } else {
+        m.hidden = false;
+      }
+    }
+
+    // sort
+    sortPC();
+    
   } else {
     print("Phone detected!");
-    try { document.getElementById("inputField").size = 10; } catch (i) {}
+
+    // Set size of 'inputField'
+    try {
+      document.getElementById("inputField").size = 10;
+      document.getElementById("outputField").size = 10;
+    } catch (i) {}
+
+    // Linebreaks
+    let pcLineBreaks = document.getElementsByClassName("brPC");
+    for (var i = 0; i < pcLineBreaks.length; i++) {
+      pcLineBreaks[i].hidden = true;
+    }
+
   }
 }
 startup();
-
-function setOutput(s) {
-  document.getElementById("outputField").value = s;
-}
-
-function getInput() {
-  return document.getElementById("inputField").value;
-}
 
 function calculate() {
   try {
@@ -77,6 +101,18 @@ function calculate() {
   }
 }
 
+function keyEvent(e) {
+  if (e.key === 'Enter') {
+    calculate();
+  }
+}
+
+function setOutput(s) {
+  document.getElementById("outputField").value = s;
+}
+function getInput() {
+  return document.getElementById("inputField").value;
+}
 function addChar(button) {
   var inputField = document.getElementById("inputField");
   if (inputField.value == "Input") {
@@ -86,15 +122,49 @@ function addChar(button) {
   inputField.value += button.innerHTML.
     replace("×", "*").replace("÷", "/");
 }
-
 function removeChar() {
   var current = document.getElementById("inputField").value;
   document.getElementById("inputField").value = current.slice(0, -1);
 }
-
 function clearInput() {
   var i = document.getElementById("inputField");
   while (i.value.length > 0){
     removeChar();
+  }
+}
+
+function sortPC() {
+  try {
+    let order = ['=', '⇐', '←', '9', '8', '7', "nl",
+                '6', '5', '4', '3', '2', '1', '0', "nl",
+                ',', '÷', '×', '-', '+', ')', '('];
+    let parent = document.getElementsByClassName("button")[0].parentNode;
+    var p = 0;
+    for(let i=0; i<order.length; i++) {
+      if (order[i] == "nl") {
+        parent.insertBefore(document.getElementsByClassName("brPC")[p], parent.firstChild);
+        p++;
+        continue;
+      }
+
+      let current = getElement(order[i]);
+      parent.insertBefore(current, parent.firstChild);
+    }
+    const outputField = document.getElementById("outputField");
+    parent = outputField.parentNode;
+    parent.insertBefore(outputField, parent.firstChild);
+    const inputField = document.getElementById("inputField");
+    parent = inputField.parentNode;
+    parent.insertBefore(inputField, parent.firstChild);
+  } catch (ignored) {}
+}
+
+function getElement(c) {
+  let buttons = document.getElementsByClassName("button");
+  for (let x=0; x<buttons.length; x++) {
+    let current = buttons[x];
+    if (current.innerHTML == c) {
+      return current;
+    }
   }
 }
